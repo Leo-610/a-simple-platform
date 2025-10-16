e3de1940b343d079cb71628cfcf4081734bfc146
Leo-610 <leo610@example.com>
2025-10-16 16:35:01 +0800

    Initial commit: Create data visualization page

---
+++ data.js
@@ -0,0 +1,12 @@
+const wordCountData = [
+    { name: 'property', value: 18 },
+    { name: 'name', value: 15 },
+    { name: 'value', value: 12 },
+    { name: 'hadoop', value: 10 },
+    { name: 'configuration', value: 8 },
+    { name: 'yarn', value: 7 },
+    { name: 'dfs', value: 6 },
+    { name: 'replication', value: 5 },
+    { name: 'resourcemanager', value: 4 },
+    { name: 'namenode', value: 3 }
+];
